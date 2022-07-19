8fa295e673dd752fe759e8e2ff9e475882cf9ab8
yangfan <506238417@qq.com>
2022-07-19 10:44:03 +0800

    init

--- util/good.go
+++ util/good.go
@@ -27,10 +27,11 @@ const (
 
 func init() {
 	_resty = resty.New().
-		SetTimeout(30 * time.Second).
-		SetRetryCount(0).
-		SetRetryWaitTime(1 * time.Second).
-		SetRetryMaxWaitTime(1 * time.Second)
+		SetTimeout(30*time.Second).
+		SetHeader("Connection", "keep-alive")
+	//SetRetryCount(0).
+	//	SetRetryWaitTime(1 * time.Second).
+	//	SetRetryMaxWaitTime(1 * time.Second)
 }
 
 func GetRestyClient() *resty.Client {
