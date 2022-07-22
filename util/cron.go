6fe115930c46f7443e763c17cefde463fad2808c
yangfan <506238417@qq.com>
2022-07-22 10:05:59 +0800

    Update cron.go

--- util/cron.go
+++ util/cron.go
@@ -5,11 +5,11 @@ import (
 )
 
 func InitTask() {
-	tk1 := toolbox.NewTask("hd_15256002129", "* 41 17 * * *", StartOrders15256002129)
-	tk2 := toolbox.NewTask("hd_13401159806", "* 41 17 * * *", StartOrders13401159806)
-	tk3 := toolbox.NewTask("hd_13155347128", "* 41 17 * * *", StartOrders13155347128)
+// 	tk1 := toolbox.NewTask("hd_15256002129", "* 41 17 * * *", StartOrders15256002129)
+// 	tk2 := toolbox.NewTask("hd_13401159806", "* 41 17 * * *", StartOrders13401159806)
+// 	tk3 := toolbox.NewTask("hd_13155347128", "* 41 17 * * *", StartOrders13155347128)
 
-	toolbox.AddTask("hd_15256002129", tk1)
-	toolbox.AddTask("hd_13401159806", tk2)
-	toolbox.AddTask("hd_13155347128", tk3)
+// 	toolbox.AddTask("hd_15256002129", tk1)
+// 	toolbox.AddTask("hd_13401159806", tk2)
+// 	toolbox.AddTask("hd_13155347128", tk3)
 }
